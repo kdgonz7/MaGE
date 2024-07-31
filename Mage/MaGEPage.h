10ffcaf880bbb104b5152ed30683a8adc1994133
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-31 11:56:55 -0400

    change the comments to normal people comments

--- Mage/MaGEPage.h
+++ Mage/MaGEPage.h
@@ -27,82 +27,39 @@ namespace MaGE {
 		const char* getFilename() { return filename; }
 		const int32_t getVersion() { return version; }
 
-		/// <summary>
-		/// Allocates a new page with a size of `this.max_size`
-		/// </summary>
-		/// <returns>the new raw page</returns>
+		// Allocates a new page with a size of `this.max_size`
 		void* allocate();
 
-		/// <summary>
-		/// Writes to the data at the offset address. This will overwrite any existing data.
-		/// </summary>
-		/// <param name="data"></param>
-		/// <param name="size"></param>
-		/// <param name="offset"></param>
+		// Writes to the data at the offset address. This will overwrite any existing data.
 		void write(const char* data, const size_t size, const size_t offset = 0, bool skip_header = true);
 
-		/// <summary>
-		/// Writes to the page where the data is 0
-		/// </summary>
-		/// <param name="info"></param>
-		/// <param name="size"></param>
+		// Writes to the page where the data is 0
 		void writeWhereFree(const char* info, const size_t size);
 
-		/// <summary>
-		/// Creates a new page file
-		/// </summary>
-		/// <param name="filename"></param>
+		// Creates a new page file
 		void dumpToFile(const char* filename);
 
-		/// <summary>
-		/// Loads this page from the specified file
-		/// </summary>
+		// Loads this page from the specified file
 		void load(const char* filename);
 
-		/// <summary>
-		/// Reads an integer from the specified offset
-		/// </summary>
-		/// <param name="offset">The offset to read from</param>
-		/// <param name="endianness">The endianness of the integer</param>
-		/// <returns></returns>
+		// Reads an integer from the specified offset
 		int32_t readInt32(const size_t offset = 0, MaGE::Endian endianness = MaGE::Endian::ENDIAN_LITTLE, bool skip_header = true);
 		int64_t readInt64(const size_t offset = 0, MaGE::Endian endianness = MaGE::Endian::ENDIAN_LITTLE) const;
 
-		/// <summary>
-		/// Writes an integer to the specified offset
-		/// </summary>
-		/// <param name="offset">The offset to write to</param>
-		/// <param name="value">The value to write</param>
-		/// <param name="endianness">The endianness of the integer</param>
-		/// <returns></returns>
+		// Writes an integer to the specified offset
 		void writeInt32(const size_t offset, const int32_t value, MaGE::Endian endianness = MaGE::Endian::ENDIAN_LITTLE, bool skip_header = true);
 		void writeInt64(const size_t offset, const int64_t value, MaGE::Endian endianness = MaGE::Endian::ENDIAN_LITTLE);
 
-		/// <summary>
-		/// Checks if the data stream has the magic number
-		/// </summary>
-		/// <param name="data"></param>
-		/// <param name="size"></param>
-		/// <returns></returns>
+		// Checks if the data stream has the magic number
 		bool dataStreamHasMagic(const char* data);
 
-		/// <summary>
-		/// Essentially `malloc(size)`
-		/// </summary>
-		/// <param name="size"></param>
-		/// <returns></returns>
+		// Essentially `malloc(size)`
 		MaGE::GE_MemoryObject *ask(size_t size);
 
-		/// <summary>
-		/// Sets the authority for this page
-		/// </summary>
-		/// <param name="info"></param>
-		/// <param name="size"></param>
+		// Sets the authority for this page
 		void setAuthority(bool info);
 		
-		/// <summary>
-		/// Ends the usage of the page (primarily for use with authority)
-		/// </summary>
+		// Ends the usage of the page (primarily for use with authority)
 		void endUsage();
 	};
 }
